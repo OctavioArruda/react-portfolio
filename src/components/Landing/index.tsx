26cf4f108bc3cf8f404234c6cf9797d053e1c226
Octavio Arruda <oneiros241@gmail.com>
2020-12-22 23:39:02 -0300

    landing - iss6

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-import { Hero, HighlightedText, NormalHeroText } from "./styles";
+import { Hero, HighlightedText, NormalHeroText, TextContainer } from "./styles";
 
 const Landing: React.FC = () => {
   return (
     <Hero>
-      {/* Todo: NormalHeroText recebe prop que define o tamanho dele */}
-      <NormalHeroText>
-        Hello, <HighlightedText>I'm Octavio</HighlightedText>
-      </NormalHeroText>
-      <NormalHeroText>
-        A creative <HighlightedText>software developer </HighlightedText>
-        based in Porto Alegre.
-      </NormalHeroText>
+      <TextContainer>
+        <NormalHeroText fontSize={6}>
+          Hello, <HighlightedText>I'm Octavio</HighlightedText>
+        </NormalHeroText>
+        <NormalHeroText fontSize={4}>
+          A creative <HighlightedText>software developer </HighlightedText>
+          based in Porto Alegre.
+        </NormalHeroText>
+      </TextContainer>
     </Hero>
   );
 };
--- src/components/Landing/styles.ts
+++ src/components/Landing/styles.ts
@@ -2,20 +2,48 @@ import styled from 'styled-components';
 
 import HeroImage from "../../assets/landing-bg.jpg";
 
+interface NomeHeroTextProps {
+  fontSize: number;
+}
+
 export const Hero = styled.section`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   width: 100vw;
   height: 100vh;
 
-  background-image: url(${HeroImage});
+  background: url(${HeroImage}) top center no-repeat;
   background-size: cover;
 
-  padding-top: 6rem;
+  padding: 6rem 2rem 0;
+
+  &::after {
+    left: 0;
+    top: 0;
+    content: '';
+
+    position: absolute;
+
+    width: 100%;
+    height: 100%;    
+
+    background-color: rgba(0, 0, 0, 0.6);
+  }
 `;
 
 export const HighlightedText = styled.span`
   color: var(--fourth-red);
 `;
 
-export const NormalHeroText = styled.p`
+export const NormalHeroText = styled.p<NomeHeroTextProps>`
   color: var(--secondary-light);
+  font-size: ${p => p.fontSize || 2}rem;
+`;
+
+export const TextContainer = styled.div`
+  text-align: center;
+  z-index: 1;
 `;
