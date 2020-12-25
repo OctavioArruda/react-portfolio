f4c247fcdc1039019372efadee9faa7be68b748e
Octavio Arruda <oneiros241@gmail.com>
2020-12-24 23:58:54 -0300

    clean up

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,6 @@ import React from "react";
 // import { Grid } from './styles';
 import Navbar from "../Navbar";
 import Landing from "../Landing";
-// import ChannelInfo from '../ChannelInfo';
-// import ChannelList from '../ChannelList';
-// import UserInfo from '../UserInfo';
-// import UserList from '../UserList';
-// import ChannelData from '../ChannelData';
 
 const Layout: React.FC = () => {
   return (
